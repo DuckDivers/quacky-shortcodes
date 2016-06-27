cf3f3600259a82bd0cee2ff688e420dc5f829600
DuckDivers <howarde@scubaboard.com>
2016-06-27 13:20:49 -0400

    Add Checkbox to disable

--- assets/anchor-options.php
+++ assets/anchor-options.php
@@ -3,7 +3,11 @@
 	<form action="options.php" method="post" id="<?php echo $plugin_id; ?>_options_form" name="<?php echo $plugin_id; ?>_options_form">
  
 	<?php settings_fields($plugin_id.'_options'); ?>
-    
+    <?php 
+		if(get_option( 'essl_active' )){
+			$checked = 'checked';}
+			else {$checked ='';};
+    	?>
     <h2>Duck Diver Smooth Scroll &raquo; Settings</h2>
     <table class="widefat">
 		<thead>
@@ -13,6 +17,14 @@
 		</thead>
 
 		<tbody>
+           <tr>
+			 <td style="padding:5px;font-family:Verdana, Geneva, sans-serif;color:#666;">
+                 <label for="essl_active">
+                     <p>Activate the Smooth Scroll Links</p>
+                     <p><input  size="10" id="active" type="checkbox" name="essl_active" value="1" <?php echo $checked;?>></p>
+                 </label>
+             </td>
+		   </tr>	
 		   <tr>
 			 <td style="padding:5px;font-family:Verdana, Geneva, sans-serif;color:#666;">
                  <label for="essl_speed">
@@ -29,15 +41,7 @@
                  </label>
              </td>
 		   </tr>
-
-
-
-
-
-		   
 		</tbody>
-		
-
 		<tfoot>
 		   <tr>
 			 <th><input type="submit" name="submit" value="Save Settings" class="button button-primary" onClick="return empty()"  /></th>
